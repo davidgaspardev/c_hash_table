ebe557f0e5c2611272eab36f563c6960391c610c
David Gaspar <david.correa@intelbras.com.br>
2023-01-14 11:39:49 -0300

    chore: create a new hashtable

---
+++ src/hashtable.c
@@ -0,0 +1,31 @@
+#include "hashtable.h"
+
+hashtable_t * create_hashtable(uint_t table_size) {
+#ifdef DEBUG_MODE
+    printf("creating hashtable");
+#endif
+    hashtable_t * hashtable = (hashtable_t*) malloc(sizeof(hashtable_t));
+
+    hashtable->cells = (cell_t *) malloc(sizeof(cell_t) * table_size);
+    hashtable->size = table_size;
+
+    return hashtable;
+}
+
+void destroy_hashtable(hashtable_t * hashtable) {
+#ifdef DEBUG_MODE
+    printf("detroing hashtable")
+#endif
+    free(hashtable->cells);
+    free(hashtable);
+}
+
+int8_t hashtable_insert(hashtable_t * hashtable, cell_t cell) {
+    hashtable->cells[0] = cell;
+
+    return 1;
+}
+
+void show_hashtable(hashtable_t * hashtable) {
+
+}
---
+++ src/hashtable.h
@@ -0,0 +1,34 @@
+#ifndef HASHTABLE_H_
+#define HASHTABLE_H_
+
+#include <stdlib.h>
+#include <stdint.h>
+
+#ifdef DEBUG_MODE
+#include <stdio.h>
+#endif
+
+typedef unsigned int uint_t;
+
+typedef struct cell {
+    char key[20];
+    void * data;
+    void * next;
+} cell_t;
+
+typedef struct hashtable {
+    cell_t* cells;
+    uint_t size;
+} hashtable_t;
+
+hashtable_t * create_hashtable(uint_t table_size);
+
+void destroy_hashtable(hashtable_t * hashtable);
+
+// Insert cell in the hashtable (WRITE/INPUT)
+int8_t hashtable_insert(hashtable_t * hashtable, cell_t cell);
+
+// Show all cell data from hashtable
+void show_hashtable(hashtable_t * hashtable);
+
+#endif
