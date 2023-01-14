a1186805827102db7ba7e324faf0e3a35a1599fc
David Gaspar <davidgaspar.dev@gmail.com>
2023-01-14 17:34:47 -0300

    chore: create new hash function

---
+++ src/hash.c
@@ -0,0 +1,24 @@
+#include "hash.h"
+
+#ifdef DEBUG_MODE
+void console(char * msg) {
+    printf("[ DEBUG ] %s\n", msg);
+}
+#endif
+
+// Hash function that takes a string as an input and returns an index between 0 and max_size
+unsigned int hash(const char* key, unsigned int max_size) {
+#ifdef DEBUG_MODE
+    char msg[50];
+    sprintf("create hash index - key: %s (max size: %d)", key, max_size);
+    console(msg);
+#endif
+    unsigned int hash = 0;
+    unsigned int i;
+
+    for (i = 0; i < strlen(key); i++) {
+        hash = hash * 31 + key[i];
+    }
+
+    return hash % max_size;
+}
---
+++ src/hash.h
@@ -0,0 +1,10 @@
+#ifndef HASH_H_
+#define HASH_H_
+
+#ifdef DEBUG_MODE
+#include <stdio.h>
+#endif
+
+unsigned int hash(const char * key, unsigned int max_size);
+
+#endif
